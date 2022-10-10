f0df9fcafecc83ce71047a44ab2aa0dcd515e424
David <75834958+1621392358@users.noreply.github.com>
2022-10-10 14:26:27 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,26 +3,26 @@
 
 int main(void)
 {
-	GPIO_InitTypeDef CPIO_Initstructuer;
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
+	GPIO_InitTypeDef CPIO_Initstructuer;					//定义GPIO结构体
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);			//打开连接GPIO外设的时钟
 	
-	CPIO_Initstructuer.GPIO_Mode = GPIO_Mode_Out_PP;
-	CPIO_Initstructuer.GPIO_Pin = GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7;
-	CPIO_Initstructuer.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOB,&CPIO_Initstructuer);
+	CPIO_Initstructuer.GPIO_Mode = GPIO_Mode_Out_PP;			//设置GPIO推挽输出模式
+	CPIO_Initstructuer.GPIO_Pin = GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7;//指定设置的GPIO端口
+	CPIO_Initstructuer.GPIO_Speed = GPIO_Speed_50MHz;			//设置GPIO频率（一般50HZ就够了）
+	GPIO_Init(GPIOB,&CPIO_Initstructuer);					//结构体初始化的最后一步
 	
 	
-	while(1)
+	while(1)								//流水灯循环函数
 	{
 		GPIO_WriteBit(GPIOB,GPIO_Pin_4,Bit_SET);	
 		Delay_ms(500);
 		GPIO_WriteBit(GPIOB,GPIO_Pin_4,Bit_SET);
-		Delay_ms(500);
+		Delay_ms(500);							//GPIO4所连接的流水灯不闪烁
 		
 		GPIO_WriteBit(GPIOB,GPIO_Pin_5,Bit_SET);
 		Delay_ms(500);
 		GPIO_WriteBit(GPIOB,GPIO_Pin_5,Bit_RESET);
-		Delay_ms(500);
+		Delay_ms(500);							//GPIO5、6、7所连接的流水灯闪烁
 		
 		GPIO_WriteBit(GPIOB,GPIO_Pin_6,Bit_SET);
 		Delay_ms(500);
